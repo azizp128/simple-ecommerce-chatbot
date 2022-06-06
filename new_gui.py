b0a70a223fa5fa8534bb6633c88fa66b908386e3
Aziz Prabowo <azizprabowo128@gmail.com>
2022-06-07 00:36:39 +0700

    try the response in product button

--- new_gui.py
+++ new_gui.py
@@ -1,17 +1,63 @@
 import PySimpleGUI as sg
+from model import *
+from predict import get_bag_of_words, classify, response
+import datetime
 
-layout = [
-    [sg.Text('My one-shot window.')],
-    [sg.Output(size=(50,25), key='-OUTPUT-')],
-    [sg.Button('Product'), sg.Button('Price'), sg.Button('Shipping'), 
-        sg.Button('Payment')],
-    [sg.In(key='-QUERY-'), sg.Submit('Send')]
-]
+def classify_and_response(a):
+    classify_ = classify(a)
+    class_name = classify_[0]
+    
+    now = datetime.datetime.now()
+    hour = now.hour
+    
+    if hour < 12 and classify_[0] == "greeting":
+        respond = f"Halo selamat pagi, {response(class_name)}"
+    elif hour < 18 and classify_[0] == "greeting":
+        respond = f"Halo selamat siang, {response(class_name)}"
+    elif hour > 18 and classify_[0] == "greeting":
+        respond = f"Halo selamat malam, {response(class_name)}"
+    else:
+        respond = f"{response(class_name)}"
+        
+    return respond
 
-window = sg.Window('Window Title', layout)
+def product_classify(a):
+    classify_ = classify(a)
+    class_name = classify_[0]
 
-while True:
-    event, values = window.read()
-    print(values['-QUERY-'])
+    if classify_[0] == 'product':
+        respond = f"{response(class_name)}"
+    
+    return respond
+  
+sg.theme('GreenTan')
+sg.SetOptions(text_color="#e4e4e4", font='opensans 11')
 
+layout = [[sg.Text("Program Chatbot Toko Online", font='sfprodisplay 25 bold')], 
+            [sg.Text('Output', size=(40,1))],
+            [sg.Output(size=(70,25), key='-OUTPUT-', font=('Helvetica 10'))],
+            [sg.Button('Product'), sg.Button('Price'), sg.Button('Shipping'), 
+                sg.Button('Payment')],
+            [sg.Text('Chat', size=(40,1))],
+            [sg.Multiline(size=(40,1), key='-QUERY-', do_not_clear=False), 
+             sg.Button('SEND', button_color=(sg.YELLOWS[0], sg.BLUES[0]), bind_return_key=True),
+             sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
+
+window = sg.Window("Chat", layout, font=('Helvetica', '13'), 
+                    default_button_element_size=(8,2), 
+                    use_default_focus=False)
+
+if __name__ == "__main__":
+  while True:
+    event, value = window.read()
+    if event == 'Product':
+        query = product_classify(value['-QUERY-'])
+        print(f"ADMIN : {query}\n")
+    if event != "EXIT":
+        break
+    if event == 'SEND':
+        query = classify_and_response(value['-QUERY-'])
+        print(f"USER : {value['-QUERY-']}\nADMIN : {query}\n")
+    
 window.close()
+    
